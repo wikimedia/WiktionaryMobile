a785ada820c8937ebb281d946272da762661b316
Herm Wong <herm.wong@gmail.com>
2011-09-06 15:56:38 -0700

    load main page in shell, stub in the features bar

--- assets/www/main.js
+++ assets/www/main.js
@@ -1,6 +1,12 @@
-function init() {
+function init() 
+{
     // the next line makes it impossible to see Contacts on the HTC Evo since it
     // doesn't have a scroll button
-    // document.addEventListener("touchmove", preventBehavior, false);
-    document.addEventListener("deviceready", deviceInfo, true);
+    // document.addEventListener("touchmove", preventBehavior, false);  
+    document.addEventListener("deviceready", loadContent, true);
+}
+
+function loadContent() 
+{
+	document.getElementById("main").src = "http://en.m.wikipedia.org";
 }
