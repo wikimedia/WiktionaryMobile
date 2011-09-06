e531b0a7d5eb8982f6298b6ab65de861453e1d16
Herm Wong <herm.wong@gmail.com>
2011-09-06 11:42:18 -0700

    shell project

---
+++ assets/www/main.js
@@ -0,0 +1,6 @@
+function init() {
+    // the next line makes it impossible to see Contacts on the HTC Evo since it
+    // doesn't have a scroll button
+    // document.addEventListener("touchmove", preventBehavior, false);
+    document.addEventListener("deviceready", deviceInfo, true);
+}
---
+++ src/org/wikipedia/WikipediaActivity.java
@@ -0,0 +1,13 @@
+package org.wikipedia;
+
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class WikipediaActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
