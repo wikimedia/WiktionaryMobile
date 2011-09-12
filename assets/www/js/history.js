3d27de41f1cf8f32b8e40c2a8586be4d6ecb1fa4
Herm Wong <herm.wong@gmail.com>
2011-09-12 14:09:35 -0700

    added app icon

--- assets/www/js/history.js
+++ assets/www/js/history.js
@@ -1,3 +1,5 @@
+//window.onpushstate = alert(window.history.current);
+
 function isHistoryMaxLimit()
 {
 	var MAX_LIMIT = 50;
@@ -17,12 +19,35 @@ function isHistoryMaxLimit()
 	});
 }
 
-showHistory()
+function getHistory()
+{
+	//alert(history.length);
+	document.getElementById("history").innerHTML = "<ul>";
+	
+	if (history.length > 0)
+	{
+		for (var i=history.length;i>=0;i--)
+		{
+			document.getElementById("history").innerHTML += "<li>";
+			document.getElementById("history").innerHTML += "";
+			document.getElementById("history").innerHTML += "</li>";	
+		}
+	}
+	else
+	{
+		document.getElementById("history").innerHTML += "<li>no items in history</li>";	
+	}
+	
+	document.getElementById("history").innerHTML += "</ul>";
+	document.getElementById("history").innerHTML += "<a href='javascript:hideHistory();'>Close</a>";
+}
+
+function showHistory()
 {
 	document.getElementById("bookmarks").style.display = "block";
 }
 
-hideHistory()
+function hideHistory()
 {
 	document.getElementById("bookmarks").style.display = "none";
 }
