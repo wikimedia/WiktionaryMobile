d79ffe48871b99c1306d5b2689cab97e1e496b3c
Anis Kadri <anis.kadri@gmail.com>
2011-10-20 23:57:52 -0700

    updating nearme callback

--- assets/www/js/geo.js
+++ assets/www/js/geo.js
@@ -2,8 +2,24 @@ function getCurrentPosition() {
   PhoneGap.exec(geoNameSuccess, geoNameFailure, "NearMePlugin", "startNearMeActivity", []);
 }
 
-function geoNameSuccess(result) {
-  console.log(result);
+function geoNameSuccess(wikipediaUrl) {
+    if(wikipediaUrl) {
+      $('#search').addClass('inProgress');
+      $.ajax({url: "http://en.m.wikipedia.org",
+              success: function(data) {
+                if(data) {
+                  $('#main').attr('src', 'http://'+wikipediaUrl);
+                } else {
+                  noConnectionMsg();
+                  navigator.app.exitApp();
+                }
+              },
+              error: function(xhr) {
+                noConnectionMsg();
+              },
+              timeout: 3000
+           });
+    }
 }
 
 function geoNameFailure(error) {
