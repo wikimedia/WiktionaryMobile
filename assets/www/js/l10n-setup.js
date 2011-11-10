466cb00e3a57c25906e8a4f415c52858efa79736
Brion Vibber <brion@pobox.com>
2011-11-09 17:15:05 -0800

    Load l10n messages from new .properties-format localization files.

--- assets/www/js/l10n-setup.js
+++ assets/www/js/l10n-setup.js
@@ -1,4 +1,53 @@
-// @todo check window.navigator.language & load necessary files
-$.each(messages, function(key, val) {
-	mw.messages.set(key, val);
+/**
+ * Validate and normalize a language code.
+ * Doesn't guarantee we know it or it's legit, but confirms the format is safe.
+ *
+ * @param string lang
+ * @returns string normalized to lowercase
+ * @throws Error on invalid input
+ */
+function normalizeLanguageCode(lang) {
+	if (typeof lang !== "string") {
+		throw new Error("Invalid type for language name");
+	}
+	if (!lang.match(/^[a-z0-9]+(-[a-z0-9]+)*$/)) {
+		throw new Error("Invalid language name format");
+	}
+	return lang.toLowerCase();
+}
+
+/**
+ * Load up messages for given language, synchronously to keep things simple.
+ * @param string lang
+ * @param callback
+ */
+function loadMessages(lang, callback) {
+	lang = normalizeLanguageCode(lang);
+	var url = 'messages/messages-' + lang + '.properties';
+	$.ajax({
+		url: url,
+		async: false,
+		dataType: 'text',
+		success: function(data) {
+			try {
+				var messages = propertiesFileReader.parse(data);
+			} catch (e) {
+				alert('Error parsing localization file for ' + lang + ': ' + e);
+				callback(null);
+			}
+			$.each(messages, function(key, val) {
+				mw.messages.set(key, val);
+			});
+		},
+		error: function(xhr, status, err) {
+			alert('Error loading localization file for ' + lang + ': ' + status);
+			callback(null);
+		}
+	});
+}
+
+loadMessages('en', function() {
+	$(document).trigger('mw-messages-ready');
 });
+
+//@todo check window.navigator.language & load necessary files
