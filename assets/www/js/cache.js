6457d1ce676996099cf30544d7db7af33b3a23b8
Brion Vibber <brion@pobox.com>
2011-12-15 11:01:41 -0800

    Cached page improvements: * avoid flash of unstyled text by hiding the iframe until stylesheets have all been replaced * replace/cache scripts so section show/hide works

--- assets/www/js/cache.js
+++ assets/www/js/cache.js
@@ -1,15 +1,46 @@
 function Application() {}
 
 Application.prototype.setRootPage = function(url) {
+	// Hide the iframe until the stylesheets are loaded,
+	// to avoid flash of unstyled text.
+	$('#main').hide();
 	var replaceRes = function() {
 		var frameDoc = $("#main")[0].contentDocument;
 		// links rel
+		var linkCount = 0;
 		$('link', frameDoc).each(function() {
+			linkCount++;
 			var em = $(this);
 			var gotLinkPath = function(linkPath) {
 				em.attr('href', linkPath.file);
+				linkCount--;
+				if (linkCount == 0) {
+					setTimeout(function() {
+						// We've loaded all the styles: show the frame now
+						$('#main').show();
+					}, 0);
+				}
 			}
 			window.plugins.urlCache.getCachedPathForURI(this.href.replace('file:', 'http:'), gotLinkPath, gotError);
+		});
+
+		// Scripts need to be replaced as well, for section show/hide.
+		$('script', frameDoc).each(function() {
+			var em = $(this),
+				src = em.attr('src');
+			if (src) {
+				if (src.substr(0, 2) == '//') {
+					src = 'http:' + src;
+				} else if (src.substr(0, 1) == '/') {
+					src = currentLocale.url + src;
+				}
+				var gotScriptPath = function(scriptPath) {
+					// Changing the src on the existing node doesn't seem to work.
+					// Replace it with a new one!
+					em.replaceWith($('<script>').attr('src', scriptPath.file));
+				}
+				window.plugins.urlCache.getCachedPathForURI(src, gotScriptPath, gotError);
+			}
 		});
 		
 		$('a[href^="/wiki/"]', frameDoc).each(function() {
