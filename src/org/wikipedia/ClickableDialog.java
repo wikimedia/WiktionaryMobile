f1747823c91baa0a176acca6dc08df00db7467f5
YuviPanda <yuvipanda@gmail.com>
2011-12-24 04:49:50 +0530

    Made entire popup dialog box clickable in near me

---
+++ src/org/wikipedia/ClickableDialog.java
@@ -0,0 +1,25 @@
+package org.wikipedia;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class ClickableDialog extends Dialog {
+
+	private View.OnClickListener clickListener;
+	
+	public ClickableDialog(Context context) {
+		super(context);
+	}
+	
+	public void setOnClickListener(View.OnClickListener listener) {
+		clickListener = listener;
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		clickListener.onClick(null);
+		return true;
+	}
+}
