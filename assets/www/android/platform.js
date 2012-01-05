1a9f7c4322ee0bdf9ee2423cfb6e5717fe7ae7c0
Brion Vibber <brion@pobox.com>
2012-01-05 11:03:53 -0800

    drop the honeycomb iframe size workaround: not needed with iframe gone

--- assets/www/android/platform.js
+++ assets/www/android/platform.js
@@ -24,31 +24,6 @@ var origOnDeviceReady = onDeviceReady;
 onDeviceReady = function() {
     document.addEventListener("backbutton", onBackButton, false);
     document.addEventListener("searchbutton", onSearchButton, false);
-
-	if (navigator.userAgent.match(/; Android [34]/)) {
-    	// The iframe doesn't stretch to fit its native size on Android 3 or 4.
-    	// Ideally we'd instead fit it within the size of the screen and allow
-    	// scrolling within it, but that seems to have bugs for now:
-		// <http://code.google.com/p/android/issues/detail?id=20184>
-    	//
-    	// As a temporary hack to get things working on Honeycomb / Ice Cream Sandwich,
-    	// we'll force the size of the frame after load and let the whole document scroll.
-		//
-		// @fixme When expanding sections, the document gets taller and may start scrolling
-		// inside as well or something and the scrolling-iframe-link-bug above will hit.
-		//
-		// @fixme Doesn't update until after page load is complete.
-		//
-		// @fixme Doesn't always update appropriately; sometimes leaves extra whitespace.
-		
-		var pingFrameSize = function() {
-	    	var $iframe = $('#main'),
-	    		doc = $iframe[0].contentDocument,
-	    		frameHeight = doc.body.scrollHeight;
-	    	$iframe.attr('height', frameHeight);
-		}
-		document.getElementById('main').addEventListener('load', pingFrameSize, true);
-	}
 	
 	origOnDeviceReady();
 };
