266a436dbf567b7f699ca865f519d130a38e881c
YuviPanda <yuvipanda@gmail.com>
2012-01-05 05:39:53 +0530

    Separate preferences class created

---
+++ assets/www/js/preferences.js
@@ -0,0 +1,60 @@
+// Preferences singleton, to access all 'preferences'
+// Data storage still uses Lawnchair
+// Call initializeDefaults before using.
+
+window.preferencesDB = {
+	// Defaults for preferences
+	// If it is a function, it should take only one param (another function)
+	// And when done, should replace itself with the default value
+	// And then call the 'success()' parameter
+	// If it is not a function, is just used as is
+	defaults: {
+		'fontSize': 'normal',
+		// The locale. Default content language + UI language
+		'locale': function(success) {
+			var defaults = this;
+			navigatorLang(function(lang) {
+				defaults.locale = normalizeLanguageCode(lang);
+				console.log('done with navigate');
+				success();
+			});
+		},
+		// The language. Only for content
+		'language': function(success) {
+			this.language = preferencesDB.get('locale').replace(/-.*?$/, '');
+			success();
+		}
+	},
+	// Ordering of default initializer functions to call
+	defaultFunctions: [
+		'locale',
+		'language'
+		],
+	// Serializes default function calls
+	initializeDefaults: function(success) {
+		var defaults = this.defaults;
+		var functions = this.defaultFunctions;
+		var curFunction = 0;
+		var recallFunction = function() {
+			curFunction += 1;
+			if(curFunction < functions.length) {
+				// We have more functions to call!
+				defaults[functions[curFunction]].apply(defaults, [recallFunction]);
+			} else {
+				// We are out of functions, let's say we succeeded
+				success();
+			}
+		};
+		defaults[functions[0]].apply(defaults, [recallFunction]);
+	},
+	get: function(pref) {
+		var stored = localStorage.getItem(pref);
+		if(!stored) {
+			return this.defaults[pref];
+		}
+		return stored;
+	},
+	set: function(pref, value) {
+		localStorage.setItem(pref, value);
+	}
+};
