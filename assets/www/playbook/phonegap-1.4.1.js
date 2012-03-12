137d942336f2db16a3bd29480cb64eb1141b5adf
Brion Vibber <brion@pobox.com>
2012-03-12 11:34:07 -0700

    update platform files for playbook

--- assets/www/playbook/phonegap-1.4.1.js
+++ assets/www/playbook/phonegap-1.4.1.js
@@ -1028,144 +1028,6 @@ var Acceleration = function(x, y, z) {
  * PhoneGap is available under *either* the terms of the modified BSD license *or* the
  * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
  *
- * Copyright (c) 2005-2010, Nitobi Software Inc.
- * Copyright (c) 2010-2011, IBM Corporation
- */
-
-/**
- * navigator.battery
- */
-(function() {
-    /**
-     * Check to see that navigator.battery has not been initialized.
-     */
-    if (typeof navigator.battery !== "undefined") {
-        return;
-    }
-
-    /**
-     * This class contains information about the current battery status.
-     * @constructor
-     */
-    var Battery = function() {
-        this._level = null;
-        this._isPlugged = null;
-        this._batteryListener = [];
-        this._lowListener = [];
-        this._criticalListener = [];
-    };
-
-    /**
-     * Registers as an event producer for battery events.
-     *
-     * @param {Object} eventType
-     * @param {Object} handler
-     * @param {Object} add
-     */
-    Battery.prototype.eventHandler = function(eventType, handler, add) {
-        var me = navigator.battery;
-        if (add) {
-            // If there are no current registered event listeners start the
-            // battery listener on native side.
-            if (me._batteryListener.length === 0 && me._lowListener.length === 0
-                    && me._criticalListener.length === 0) {
-                PhoneGap.exec(me._status, me._error, "Battery", "start", []);
-            }
-
-            // Register the event listener in the proper array
-            if (eventType === "batterystatus") {
-                var pos = me._batteryListener.indexOf(handler);
-                if (pos === -1) {
-                    me._batteryListener.push(handler);
-                }
-            } else if (eventType === "batterylow") {
-                var pos = me._lowListener.indexOf(handler);
-                if (pos === -1) {
-                    me._lowListener.push(handler);
-                }
-            } else if (eventType === "batterycritical") {
-                var pos = me._criticalListener.indexOf(handler);
-                if (pos === -1) {
-                    me._criticalListener.push(handler);
-                }
-            }
-        } else {
-            // Remove the event listener from the proper array
-            if (eventType === "batterystatus") {
-                var pos = me._batteryListener.indexOf(handler);
-                if (pos > -1) {
-                    me._batteryListener.splice(pos, 1);
-                }
-            } else if (eventType === "batterylow") {
-                var pos = me._lowListener.indexOf(handler);
-                if (pos > -1) {
-                    me._lowListener.splice(pos, 1);
-                }
-            } else if (eventType === "batterycritical") {
-                var pos = me._criticalListener.indexOf(handler);
-                if (pos > -1) {
-                    me._criticalListener.splice(pos, 1);
-                }
-            }
-
-            // If there are no more registered event listeners stop the battery
-            // listener on native side.
-            if (me._batteryListener.length === 0 && me._lowListener.length === 0
-                    && me._criticalListener.length === 0) {
-                PhoneGap.exec(null, null, "Battery", "stop", []);
-            }
-        }
-    };
-
-    /**
-     * Callback for battery status
-     *
-     * @param {Object} info         keys: level, isPlugged
-     */
-    Battery.prototype._status = function(info) {
-        if (info) {
-            var me = this;
-            if (me._level != info.level || me._isPlugged != info.isPlugged) {
-                // Fire batterystatus event
-                PhoneGap.fireWindowEvent("batterystatus", info);
-
-                // Fire low battery event
-                if (info.level == 20 || info.level == 5) {
-                    if (info.level == 20) {
-                        PhoneGap.fireWindowEvent("batterylow", info);
-                    }
-                    else {
-                        PhoneGap.fireWindowEvent("batterycritical", info);
-                    }
-                }
-            }
-            me._level = info.level;
-            me._isPlugged = info.isPlugged;
-        }
-    };
-
-    /**
-     * Error callback for battery start
-     */
-    Battery.prototype._error = function(e) {
-        console.log("Error initializing Battery: " + e);
-    };
-
-    PhoneGap.addConstructor(function() {
-        if (typeof navigator.battery === "undefined") {
-            navigator.battery = new Battery();
-            PhoneGap.addWindowEventHandler("batterystatus", navigator.battery.eventHandler);
-            PhoneGap.addWindowEventHandler("batterylow", navigator.battery.eventHandler);
-            PhoneGap.addWindowEventHandler("batterycritical", navigator.battery.eventHandler);
-        }
-    });
-
-}());
-
-/*
- * PhoneGap is available under *either* the terms of the modified BSD license *or* the
- * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
- *
  * Copyright (c) 2005-2011, Nitobi Software Inc.
  * Copyright (c) 2010-2011, IBM Corporation
  */
@@ -1399,7 +1261,7 @@ var Camera = Camera || (function() {
             }
         }
 
-        var destinationType = DestinationType.DATA_URL;
+        var destinationType = DestinationType.FILE_URI;
         if (options.destinationType) {
             destinationType = options.destinationType;
         }
@@ -5012,8 +4874,8 @@ if (!window.phonegap) { window.phonegap = {}; }
         // WebWorks platforms (phone/tablet).
         plugins = {};
 
-	phonegap.WebWorksPluginManager = function () {
-	};
+    phonegap.WebWorksPluginManager = function () {
+    };
 
     phonegap.WebWorksPluginManager.prototype.exec = function (win, fail, clazz, action, args) {
         if (plugins[clazz]) {
@@ -5022,6 +4884,171 @@ if (!window.phonegap) { window.phonegap = {}; }
 
         return {"status" : PhoneGap.callbackStatus.CLASS_NOT_FOUND_EXCEPTION, "message" : "Class " + clazz + " cannot be found"};
     };
+}());
+
+/*
+ * PhoneGap is available under *either* the terms of the modified BSD license *or* the
+ * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
+ *
+ * Copyright (c) 2005-2010, Nitobi Software Inc.
+ * Copyright (c) 2010-2011, IBM Corporation
+ */
+
+/**
+ * navigator.battery
+ */
+(function() {
+    /**
+     * Check to see that navigator.battery has not been initialized.
+     */
+    if (typeof navigator.battery !== "undefined") {
+        return;
+    }
+    /**
+     * This class contains information about the current battery status.
+     * @constructor
+     */
+    var Battery = function() {
+        this._level = null;
+        this._isPlugged = null;
+        this._batteryListener = [];
+        this._lowListener = [];
+        this._criticalListener = [];
+        // Register one listener to each of level and state change events using WebWorks API.
+        blackberry.system.event.deviceBatteryStateChange(this._stateChange);
+        blackberry.system.event.deviceBatteryLevelChange(this._levelChange);
+    };
+
+    /**
+     * Registers as an event producer for battery events.
+     *
+     * @param {Object} eventType
+     * @param {Object} handler
+     * @param {Object} add
+     */
+    Battery.prototype.eventHandler = function(eventType, handler, add) {
+        var me = navigator.battery;
+        if (add) {
+            // Register the event listener in the proper array
+            if (eventType === "batterystatus") {
+                var pos = me._batteryListener.indexOf(handler);
+                if (pos === -1) {
+                    me._batteryListener.push(handler);
+                }
+            } else if (eventType === "batterylow") {
+                var pos = me._lowListener.indexOf(handler);
+                if (pos === -1) {
+                    me._lowListener.push(handler);
+                }
+            } else if (eventType === "batterycritical") {
+                var pos = me._criticalListener.indexOf(handler);
+                if (pos === -1) {
+                    me._criticalListener.push(handler);
+                }
+            }
+        } else {
+            // Remove the event listener from the proper array
+            if (eventType === "batterystatus") {
+                var pos = me._batteryListener.indexOf(handler);
+                if (pos > -1) {
+                    me._batteryListener.splice(pos, 1);
+                }
+            } else if (eventType === "batterylow") {
+                var pos = me._lowListener.indexOf(handler);
+                if (pos > -1) {
+                    me._lowListener.splice(pos, 1);
+                }
+            } else if (eventType === "batterycritical") {
+                var pos = me._criticalListener.indexOf(handler);
+                if (pos > -1) {
+                    me._criticalListener.splice(pos, 1);
+                }
+            }
+        }
+    };
+
+    /**
+     * Callback for battery state change using WebWorks API
+     *
+     * @param {Object} state
+     */
+    Battery.prototype._stateChange = function(state) {
+        var me = navigator.battery;
+        if (state === 2 || state === 3) { // state is either CHARGING or UNPLUGGED
+          var info = {
+            "level":me._level,
+            "isPlugged":me._isPlugged
+          };
+
+          if (state === 2 && (me._isPlugged === false || me._isPlugged === null)) {
+            me._isPlugged = info.isPlugged = true;
+            me._fire('status', info);
+          } else if (state === 3 && (me._isPlugged === true || me._isPlugged === null)) {
+            me._isPlugged = info.isPlugged = false;
+
+            me._fire('status', info);
+          }
+        }
+    };
+
+    /**
+     * Callback for battery level change using WebWorks API
+     *
+     * @param {Object} level
+     */
+    Battery.prototype._levelChange = function(level) {
+        var me = navigator.battery;
+
+        if (level != me._level) {
+          me._level = level;
+          var info = {
+            "level":me._level,
+            "isPlugged":me._isPlugged
+          };
+
+          // Fire off the basic battery status change event listeners.
+          me._fire('status', info);
+
+          // Fire low battery events if applicable
+          if (level == 20 || level == 5) {
+              if (level == 20) {
+                me._fire('low', info);
+              } else {
+                me._fire('critical', info);
+              }
+          }
+        }
+    };
+
+    /**
+     * Helper function to fire all listeners of a type.
+     *
+     * @param {Object} type
+     * @param {Object} data
+     */
+    Battery.prototype._fire = function(type, data) {
+      var targetAr = '_batteryListener';
+
+      if (type == 'critical') {
+        targetAr = '_criticalListener';
+      } else if (type == 'low') {
+        targetAr = '_lowListener';
+      }
+      for (var i = 0, l = this[targetAr].length; i < l; i++) {
+        this[targetAr][i](data);
+      }
+    };
+
+    PhoneGap.addConstructor(function() {
+
+        if (typeof navigator.battery === "undefined") {
+            navigator.battery = new Battery();
+            PhoneGap.addWindowEventHandler("batterystatus", navigator.battery.eventHandler);
+            PhoneGap.addWindowEventHandler("batterylow", navigator.battery.eventHandler);
+            PhoneGap.addWindowEventHandler("batterycritical", navigator.battery.eventHandler);
+        }
+    });
+
 }());
 
 
@@ -5042,7 +5069,7 @@ phonegap.PluginManager = (function (webworksPluginManager) {
         retInvalidAction = { "status" : PhoneGap.callbackStatus.INVALID_ACTION, "message" : "Action not found" },
         retAsyncCall = { "status" : PhoneGap.callbackStatus.NO_RESULT, "message" : "WebWorks Is On It" },
         cameraAPI = {
-            execute: function (action, args, win, fail) {
+            execute: function (webWorksResult, action, args, win, fail) {
                 switch (action) {
                 case 'takePicture':
                     blackberry.media.camera.takePicture(win, fail, fail);
@@ -5055,11 +5082,14 @@ phonegap.PluginManager = (function (webworksPluginManager) {
             execute: function (webWorksResult, action, args, win, fail) {
                 if (action === 'getDeviceInfo') {
                     return {"status" : PhoneGap.callbackStatus.OK,
-                            "message" : {"version" : blackberry.system.softwareVersion,
-                            "name" : blackberry.system.model,
-                            "uuid" : blackberry.identity.PIN,
-                            "platform" : "PlayBook",
-                            "phonegap" : "1.3.0"}};
+                            "message" : {
+                                "version" : blackberry.system.softwareVersion,
+                                "name" : blackberry.system.model,
+                                "uuid" : blackberry.identity.PIN,
+                                "platform" : "PlayBook",
+                                "phonegap" : "1.4.1"
+                            }
+                    };
                 }
                 return retInvalidAction;
             }
@@ -5076,7 +5106,7 @@ phonegap.PluginManager = (function (webworksPluginManager) {
             }
         },
         mediaAPI = {
-            execute: function (action, args, win, fail) {
+            execute: function (webWorksResult, action, args, win, fail) {
                 if (!args.length) {
                     return {"status" : 9, "message" : "Media Object id was not sent in arguments"};
                 }
@@ -5164,7 +5194,7 @@ phonegap.PluginManager = (function (webworksPluginManager) {
                 case 'release':
                     if (audio) {
                         audioObjects[id] = undefined;
-						audio.src = undefined;
+                        audio.src = undefined;
                         //delete audio;
                     }
 
@@ -5178,7 +5208,7 @@ phonegap.PluginManager = (function (webworksPluginManager) {
             }
         },
         mediaCaptureAPI = {
-            execute: function (action, args, win, fail) {
+            execute: function (webWorksResult, action, args, win, fail) {
                 var limit = args[0],
                     pictureFiles = [],
                     captureMethod;
@@ -5259,16 +5289,16 @@ phonegap.PluginManager = (function (webworksPluginManager) {
 
         notificationAPI = {
             execute: function (webWorksResult, action, args, win, fail) {
-				if (args.length !== 3) {
-					return {"status" : 9, "message" : "Notification action - " + action + " arguments not found"};
+                if (args.length !== 3) {
+                  return {"status" : 9, "message" : "Notification action - " + action + " arguments not found"};
 
-				}
-				
-				//Unpack and map the args
+                }
+                
+                //Unpack and map the args
                 var msg = args[0],
                     title = args[1],
-					btnLabel = args[2],
-					btnLabels;
+                    btnLabel = args[2],
+                    btnLabels;
 
                 switch (action) {
                 case 'alert':
@@ -5304,8 +5334,8 @@ phonegap.PluginManager = (function (webworksPluginManager) {
         //We got a sync result or a not found from WW that we can pass on to get a native mixin
         //For async calls there's nothing to do
         if ((wwResult.status === PhoneGap.callbackStatus.OK || 
-		        wwResult.status === PhoneGap.callbackStatus.CLASS_NOT_FOUND_EXCEPTION) &&
-			    plugins[clazz]) {
+          wwResult.status === PhoneGap.callbackStatus.CLASS_NOT_FOUND_EXCEPTION) &&
+          plugins[clazz]) {
             return plugins[clazz].execute(wwResult.message, action, args, win, fail);
         }
 
