97b883eb2efa9d35204a80c03ec3d331a2ea1b43
Brion Vibber <brion@pobox.com>
2012-03-12 15:28:45 -0700

    basic share menu for Playbook: option to open in browser
    Invoke stuff for email and other messaging doesn't seem to be available on Playbook even in version 2.0.... might want to add more for Blackberry OS support though.

--- assets/www/playbook/platform.js
+++ assets/www/playbook/platform.js
@@ -12,3 +12,20 @@ chrome.openExternalLink = function(url) {
 	blackberry.invoke.invoke(blackberry.invoke.APP_BROWSER,
 		new blackberry.invoke.BrowserArguments(url));
 }
+
+function sharePage() {
+	var title = app.getCurrentTitle(),
+	url = app.getCurrentUrl().replace(/\.m\.wikipedia/, '.wikipedia');
+	
+	// @fixme add more items?
+	popupMenu([
+		mw.msg('share-open-browser'),
+		mw.msg('menu-cancel')
+	], function(val, index) {
+		if (index == 0) {
+			chrome.openExternalLink(url);
+		}
+	}, {
+		cancelButtonIndex: 1
+	});
+}
