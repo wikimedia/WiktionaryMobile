f6e4fc9bf6594c1852283e5c2a31843ac3e34118
4ndreaSt4gi <stagi.andrea@gmail.com>
2012-03-22 04:19:07 +0100

    Add links to licenses

--- assets/www/android/platform.js
+++ assets/www/android/platform.js
@@ -41,13 +41,14 @@ function setPageActionsState(state) {
 }
 
 window.CREDITS = [
-	"<a href='http://phonegap.com'>PhoneGap</a>, Apache License 2.0",
-	"<a href='http://jquery.com'>jQuery</a>, MIT License",
-	"<a href='http://leaflet.cloudmade.com/'>Leaflet.js</a>, 2-Clause BSD License",
-	"<a href='http://zeptojs.com'>Zepto</a>, MIT License",
-	"<a href='http://cubiq.org/iscroll-4'>iScroll</a>, MIT License",
-	"<a href='http://twitter.github.com/hogan.js/'>Hogan.js</a>, Apache License 2.0"
+	"<a href='http://phonegap.com'>PhoneGap</a>, <a href='http://www.apache.org/licenses/LICENSE-2.0.html'>Apache License 2.0</a>",
+	"<a href='http://jquery.com'>Query</a>, <a href='http://www.opensource.org/licenses/MIT'>MIT License</a>",
+	"<a href='http://leaflet.cloudmade.com/'>Leaflet.js</a>, <a href='http://www.opensource.org/licenses/bsd-license.php'>2-Clause BSD License</a>",
+	"<a href='http://zeptojs.com'>Zepto</a>, <a href='http://www.opensource.org/licenses/MIT'>MIT License</a>",
+	"<a href='http://cubiq.org/iscroll-4'>iScroll</a>, <a href='http://www.opensource.org/licenses/MIT'>MIT License</a>",
+	"<a href='http://twitter.github.com/hogan.js/'>Hogan.js</a>, <a href='http://www.apache.org/licenses/LICENSE-2.0.html'>Apache License 2.0</a>"
 	];
+
 
 chrome.addPlatformInitializer(function() {
 	$('html').addClass('android');
