bcc13d5d7e5183a815e4cec3787a305708a10a53
YuviPanda <yuvipanda@gmail.com>
2012-04-06 05:36:30 +0530

    Refactor to make page's source more generic

--- assets/www/js/page.js
+++ assets/www/js/page.js
@@ -1,33 +1,13 @@
 (function() {
-	window.Page = function(title, sections) { 
-		var lastCollapsibleSection = {subSections: []};
-		this.sections = [];
-		var that = this;
-		$.each(sections, function(index, section) {
-			console.log(section);
-			if(section.id === 0) {
-				// Lead Section
-				// We should also make sure that if there is a lead followed by
-				// h3, h4, etc they all fold into the lead
-				// Not sure why a page would do this though
-				section.subSections = [];
-				that.lead = section;
-				lastCollapsibleSection = section;
-				return;
-			} 
-			// Only consider leve 2 sections as 'sections'
-			// Group *all* subsections under them, no matter which level they are at
-			if(section.level == 2) {
-				section.subSections = [];
-				lastCollapsibleSection = section;
-				that.sections.push(section);
-			} else {
-				lastCollapsibleSection.subSections.push(section);
-			}
-
-		});
+	window.Page = function(title, lead, sections) { 
 		this.title = title;
+		this.lead = lead;
+		this.sections = sections;
 	};
+
+	Page.deserialize = function(data) {
+		return new Page(data.title, data.lead, data.sections);
+	}
 
 	Page.requestFromTitle = function(title) {
 		var d = $.Deferred();
@@ -42,7 +22,33 @@
 		});
 
 		request.done(function(data) {
-			var page = new Page(title, data.mobileview.sections);
+			var lead = {};
+			var sections = [];
+			var lastCollapsibleSection = {subSections: []};
+			$.each(data.mobileview.sections, function(index, section) {
+				console.log(section);
+				if(section.id === 0) {
+					// Lead Section
+					// We should also make sure that if there is a lead followed by
+					// h3, h4, etc they all fold into the lead
+					// Not sure why a page would do this though
+					section.subSections = [];
+					lead = section;
+					lastCollapsibleSection = section;
+					return;
+				} 
+				// Only consider leve 2 sections as 'sections'
+				// Group *all* subsections under them, no matter which level they are at
+				if(section.level == 2) {
+					section.subSections = [];
+					lastCollapsibleSection = section;
+					sections.push(section);
+				} else {
+					lastCollapsibleSection.subSections.push(section);
+				}
+
+			});
+			var page = new Page(title, lead, sections);
 			d.resolve(page);
 		});
 
@@ -52,6 +58,11 @@
 	Page.prototype.toHtml = function() {
 		var contentTemplate = templates.getTemplate('content-template');
 		return contentTemplate.render(this);
+	}
+
+	Page.prototype.serialize = function() {
+		// Be more specific later on, but for now this does :)
+		return JSON.stringify(this);
 	}
 
 	function makeAPIRequest(params) {
