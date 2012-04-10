c6504f982e88607748dab8ac7eef1d12339060a6
Patrick Hayes <pfhayes@gmail.com>
2012-04-10 14:15:03 -0400

    Fixing MobileFrontend.toggle for v1.0.1b

--- assets/www/js/wiktionary.js
+++ assets/www/js/wiktionary.js
@@ -10,7 +10,7 @@ window.wiktionary = function() {
 	function afterPageLoad() {
 		// If no section is open, then toggle open the first section
 		if ($('.openSection').length === 0 && $('#section_1').length > 0) {
-			MobileFrontend.toggle.wm_toggle_section(1);
+			MobileFrontend.wm_toggle_section(1);
 		}
 	}
 
