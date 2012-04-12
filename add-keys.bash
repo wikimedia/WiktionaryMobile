f1b1d7179a840e2cb8f71792e2ca7a446a5d9e7c
YuviPanda <yuvipanda@gmail.com>
2012-04-12 07:42:26 +0530

    Fixed bash misunderstanding

--- add-keys.bash
+++ add-keys.bash
@@ -7,6 +7,6 @@ read FBKEY
 echo -n "Enter the RIL API Key: "
 read RILKEY
 
-sed -i -e "s/\[RIL-API-KEY\]/$RILKEY/" Wikipedia-iOS/Plugins/ReadItLater/ReadItLaterLite.m
-sed -i -e "s/\[FB-APP-ID\]/$FBKEY/" Wikipedia-iOS/Wikipedia-iOS-Info.plist
-sed -i -e "s/\[FB-APP-ID\]/$FBKEY/" assets/www/ios/platform.js
+sed -i.bak "s/\[RIL-API-KEY\]/$RILKEY/" Wikipedia-iOS/Plugins/ReadItLater/ReadItLaterLite.m
+sed -i.bak "s/\[FB-APP-ID\]/$FBKEY/" Wikipedia-iOS/Wikipedia-iOS-Info.plist
+sed -i.bak "s/\[FB-APP-ID\]/$FBKEY/" assets/www/ios/platform.js
