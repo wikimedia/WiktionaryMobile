75c12ef30359b7f7a2ac17a427acb885394b19ea
YuviPanda <yuvipanda@gmail.com>
2012-04-14 01:20:13 +0530

    More 'Proper' Section expansion implementation

--- assets/www/js/chrome.js
+++ assets/www/js/chrome.js
@@ -41,13 +41,13 @@ window.chrome = function() {
 			var $contentBlock = $("#content_" + sectionID);
 			if($contentBlock.attr('data-populated') != 'true') {
 				var sectionHtml = app.curPage.getSectionHtml(sectionID);
-				$contentBlock.html(sectionHtml).attr('data-populated', 'true').slideDown();
+				$contentBlock.html(sectionHtml).attr('data-populated', 'true');
+			} 
+
+			if($contentBlock.hasClass('openSection')) {
+				$contentBlock.slideUp().removeClass('openSection');
 			} else {
-				if($contentBlock.is(':visible')) {
-					$contentBlock.slideUp();
-				} else {
-					$contentBlock.slideDown();
-				}
+				$contentBlock.slideDown().addClass('openSection');
 			}
 		});
 	}
