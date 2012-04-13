ea5d770bf8bbf9e00684a5fac905d02e325496d0
Patrick Hayes <pfhayes@gmail.com>
2012-04-13 14:33:51 -0400

    Trying to get starting words working on 4.0
    Fixing NPE with StartingWordAccessor
    
    Making class private

--- src/org/wiktionary/WiktionaryActivity.java
+++ src/org/wiktionary/WiktionaryActivity.java
@@ -10,14 +10,27 @@ import android.util.Log;
 import com.phonegap.DroidGap;
 
 public class WiktionaryActivity extends DroidGap {
+    // Wrapper to pass Intent word to phonegap
+    private static class StartingWordAccessor {
+      private String wordToShow;
+      public StartingWordAccessor(String word) {
+        this.wordToShow = word;
+      }
+      public String getStartingWord() {
+        return this.wordToShow;
+      }
+    }
+
     /** Called when the activity is first created. */
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Need this for addJavascriptInterface to work
+        super.init();
           
         // Check if we were started by another app's Intent
-        boolean startedFromAnotherApp = false;
         String wordToShow = null;
         
         Intent startingIntent = this.getIntent();
@@ -26,15 +39,12 @@ public class WiktionaryActivity extends DroidGap {
     		if (extraParams != null) {
     			String value = extraParams.getString(Intent.EXTRA_TEXT);
     			if (value != null && !value.trim().equals("")) {
-    				startedFromAnotherApp = true;
     				wordToShow = value;
     			}
     		}
         }
 
-        String startingUrl = "file:///android_asset/www/index.html";
-        
-        if (startedFromAnotherApp) { // Specify the word display on startup
+        if (wordToShow != null) { // Specify the word display on startup
 
           // See if we came from another wiki app (and thus have to parse a url)
           String wikiUrl = "/wiki/";
@@ -44,16 +54,18 @@ public class WiktionaryActivity extends DroidGap {
     	  }
 
           wordToShow = URLEncoder.encode(wordToShow);
-          startingUrl += "?define=" + wordToShow;
 
           Log.d("WiktionaryActivity", "App opened with Intent. Word: " + wordToShow);
         }
-
+        
+        String startingUrl = "file:///android_asset/www/index.html";
+        appView.addJavascriptInterface(new StartingWordAccessor(wordToShow),
+          "startingWordAccessor");
         super.loadUrl(startingUrl);
         
         String currentUA = this.appView.getSettings().getUserAgentString();
-        
         this.appView.getSettings().setUserAgentString("WiktionaryMobile/1.1 " + currentUA);
     }
     
 }
+
