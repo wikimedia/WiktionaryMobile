0ae086df2821cae81d372116079e7baeacabab4d
Brion Vibber <brion@pobox.com>
2012-04-23 22:48:13 +0000

    fix styles, scrolling, show/hide section icons for WinPhone

--- assets/www/winphone/platform.js
+++ assets/www/winphone/platform.js
@@ -1,8 +1,14 @@
 // ...
 console.log('WinPhone platform.js');
 
+$('html').addClass('winphone');
+
 //$('<script>').attr('src', 'winphone/cordova-1.6.1.js').appendTo('html');
 document.write('<script src="winphone/cordova-1.6.1.js"></script>');
 
 // http://bugs.jquery.com/ticket/10660
 $.support.cors = true;
+
+$(document).bind('touchstart', function(event) {
+	event.preventDefault();
+});
